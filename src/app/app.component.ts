9bfd61c51a88a3229b202f5ce3a8df4f14e7177a
Bulat Asadullin <Bulat_Asadullin@epam.com>
2022-06-02 13:53:07 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PeopleService} from "./people.service";
+import {FormControl, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  resData : object | undefined;
+  arrayOfPeople: Array<any> | undefined
+  nameForm: any = {
+    name: ''
+  }
+  name: string | undefined
+  constructor(private peopleService: PeopleService) {
+    this.peopleService.getPeople().subscribe((data) => this.arrayOfPeople = data.results)
+  }
+  form: FormGroup = new FormGroup({
+    name: new FormControl('')
+  })
+  showlog() {
+    console.log(this.arrayOfPeople)
+  }
+  printForm(){
+    this.name = this.name1.value
+    this.form.reset()
+  }
+  get name1(): FormControl {
+    return this.form.get('name') as FormControl
+  }
+
 }
